f0c1c2aeaf17f2fa2d8a9409ed42a54f4c564751
Nikushin I.M <ivan.nikushin.99@mail.ru>
2024-01-22 12:36:57 +0300

    Добавлен тест к классу GetOperator и добавлена проверка на null в методе этого класса

--- src/test/java/calculator/operator/GetOperatorTest.java
+++ src/test/java/calculator/operator/GetOperatorTest.java
@@ -1,20 +1,51 @@
 package calculator.operator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static calculator.operator.Operator.*;
 
+@ExtendWith(MockitoExtension.class)
 class GetOperatorTest {
-    GetOperator getOperator = new GetOperator();
+
+    @Spy
+    private GetOperator getOperator;
 
     @Test
-    void apply() {
-        assertEquals(getOperator.apply("+"), Operator.SUM);
-        assertEquals(getOperator.apply("-"), Operator.DIFFERENCE);
-        assertEquals(getOperator.apply("*"), Operator.MULTIPLICATION);
-        assertEquals(getOperator.apply("/"), Operator.DIVISION);
-        assertNull(getOperator.apply("12"));
-        assertNull(getOperator.apply("re"));
+    void getSumm() {
+        assertEquals(SUM, getOperator.apply("+"));
+    }
+
+    @Test
+    void getDifference() {
+        assertEquals(DIFFERENCE, getOperator.apply("-"));
+    }
+
+    @Test
+    void getMultiplication() {
+        assertEquals(MULTIPLICATION, getOperator.apply("*"));
+    }
+
+    @Test
+    void getDivision() {
+        assertEquals(DIVISION, getOperator.apply("/"));
+    }
+
+    @Test
+    void getNullWithUnknownOperator() {
+        assertNull(getOperator.apply("a"));
+    }
+
+    @Test
+    void getNullForEmptyString() {
         assertNull(getOperator.apply(""));
+    }
+
+    @Test
+    void getNullWhenNull() {
+        assertNull(getOperator.apply(null));
     }
 }

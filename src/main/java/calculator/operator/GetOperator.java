5b64823a78684616f415c4a5513c1c8e3333a784
Nikushin I.M <ivan.nikushin.99@mail.ru>
2024-01-22 12:36:57 +0300

    Добавлен тест к классу GetOperator и добавлена проверка на null в методе этого класса

--- src/main/java/calculator/operator/GetOperator.java
+++ src/main/java/calculator/operator/GetOperator.java
@@ -5,11 +5,13 @@ import java.util.function.Function;
 public class GetOperator implements Function<String, Operator> {
     @Override
     public Operator apply(String s) {
-        switch (s) {
-            case "+": return Operator.SUM;
-            case "-": return Operator.DIFFERENCE;
-            case "*": return Operator.MULTIPLICATION;
-            case "/": return Operator.DIVISION;
+        if (s != null) {
+            switch (s) {
+                case "+": return Operator.SUM;
+                case "-": return Operator.DIFFERENCE;
+                case "*": return Operator.MULTIPLICATION;
+                case "/": return Operator.DIVISION;
+            }
         }
         return null;
     }
